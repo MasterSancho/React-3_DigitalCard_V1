ecf677b2784e247fb388c45362cc7f93ff277306
Alex M <1maltsevalex@gmail.com>
2021-07-17 22:57:23 +0300

    17.7.21 -> Update 2

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -14,20 +14,25 @@ const users = {
   whatsapp: 'https://api.whatsapp.com/send?phone=9720547537573',
   mail: 'mailto:arieli.shai@remax.co.il',
   waze: 'https://waze.com/ul/hsv8z4c0xq',
-  facebook: '',
-  madlan: '',
+  facebook: 'https://www.facebook.com/shaiarieliremax',
+  madlan: 'https://www.madlan.co.il/agent/re_agent_823uQZTpL2',
   maagar_nehasim: 'https://www.remax-israel.com/arielis',
-  atar_ezer: '',
-  atar_mektsuim: '',
-  atar_minAtonim: '',
-  atar_daNnehita: '',
-  atar_amadrihLeMehira: '',
+  atar_ezerGovmap: 'https://www.govmap.gov.il/',
+  atar_mektsuim:
+   'https://www.tivuch1.co.il/%D7%A1%D7%95%D7%9B%D7%9F/%D7%A9%D7%99-%D7%90%D7%A8%D7%99%D7%90%D7%9C%D7%99/',
+  atar_minAtonim: 'https://theselected.walla.co.il/item/3405214',
+  atar_daNnehita: 'https://www.remax-one.info/',
+  atar_amadrihLeMehira:
+   'http://online.flipbuilder.com/hckf/bjuu/mobile/index.html#p=1',
  },
  icons_share: {
-  facebook: '',
-  whatsapp: '',
-  sms: '',
-  mail: '',
+  facebook:
+   'https://www.facebook.com/sharer.php?u=https%3A%2F%2Fremax.your-app.website%2Farieli-shai%2F',
+  whatsapp:
+   'https://api.whatsapp.com/send?text=*%D7%A9%D7%99%20%D7%90%D7%A8%D7%99%D7%90%D7%9C%D7%99%20-%20%D7%99%D7%95%D7%A2%D7%A5%20%D7%94%D7%A0%D7%93%D7%9C%22%D7%9F%20%D7%94%D7%90%D7%99%D7%A9%D7%99%20%D7%A9%D7%9C%D7%9A*%20https%3A%2F%2Fremax.your-app.website%2Farieli-shai%2F',
+  sms: 'sms://?&amp;body=%D7%A9%D7%99%20%D7%90%D7%A8%D7%99%D7%90%D7%9C%D7%99%20-%20%D7%99%D7%95%D7%A2%D7%A5%20%D7%94%D7%A0%D7%93%D7%9C%22%D7%9F%20%D7%94%D7%90%D7%99%D7%A9%D7%99%20%D7%A9%D7%9C%D7%9A%20https%3A%2F%2Fremax.your-app.website%2Farieli-shai%2F',
+  mail:
+   'mailto:?subject=%D7%A9%D7%99%20%D7%90%D7%A8%D7%99%D7%90%D7%9C%D7%99%20-%20%D7%99%D7%95%D7%A2%D7%A5%20%D7%94%D7%A0%D7%93%D7%9C%22%D7%9F%20%D7%94%D7%90%D7%99%D7%A9%D7%99%20%D7%A9%D7%9C%D7%9A&amp;body=https%3A%2F%2Fremax.your-app.website%2Farieli-shai%2F',
  },
 };
 
